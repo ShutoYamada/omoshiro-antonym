c1accab5149e4be92a736ece65f8fa0180d96ecb
ShutoYamada <stym1122@gmail.com>
2026-01-30 11:25:14 +0900

    Change variables.tf multiline

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,9 +1,19 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "asia-northeast1" }
-variable "service_name" { type = string default = "omoshiro-api" }
+variable "project_id" { 
+    type = string 
+}
+variable "region"     { 
+    type = string 
+    default = "asia-northeast1" 
+}
+variable "service_name" { 
+    type = string 
+    default = "omoshiro-api" 
+}
 
 # Cloud Runが参照するコンテナイメージ
-variable "image" { type = string }
+variable "image" { 
+    type = string 
+}
 
 # MVPなので unauth を許す
 variable "allow_unauthenticated" { type = bool default = true }
